43b45b3d28e1647f491860cc1368abd25d348731
yotam freund <freund.yotam@gmail.com>
2021-12-09 22:01:05 +0200

    Adding empty template of amout_set

---
+++ amount_set.c
@@ -0,0 +1,65 @@
+
+
+struct AmountSet {
+
+};
+
+AmountSet asCreate(CopyASElement copyElement, FreeASElement freeElement, CompareASElements compareElements)
+{
+
+}
+
+void asDestroy(AmountSet set)
+{
+
+}
+
+AmountSet asCopy(AmountSet set)
+{
+
+}
+
+int asGetSize(AmountSet set)
+{
+
+}
+
+bool asContains(AmountSet set, ASElement element)
+{
+
+}
+
+AmountSetResult asGetAmount(AmountSet set, ASElement element, double *outAmount)
+{
+
+}
+
+AmountSetResult asRegister(AmountSet set, ASElement element)
+{
+
+}
+
+AmountSetResult asChangeAmount(AmountSet set, ASElement element, const double amount)
+{
+
+}
+
+AmountSetResult asDelete(AmountSet set, ASElement element)
+{
+
+}
+
+AmountSetResult asClear(AmountSet set)
+{
+
+}
+
+ASElement asGetFirst(AmountSet set)
+{
+
+}
+
+ASElement asGetNext(AmountSet set)
+{
+
+}
