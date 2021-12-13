08ccd8fb1309d8c8fd23f7e6ea4d5bb4ce3a08d9
yotam freund <freund.yotam@gmail.com>
2021-12-13 22:38:15 +0200

    Refactoring and debuging code

--- amount_set_str.c
+++ amount_set_str.c
@@ -45,28 +45,40 @@ void asDestroy(AmountSet set)
     free(set);
 }
 
+static Node* linkNewNode(Node** nextPointer, const char* string, int amount){
+    Node* node = (Node*) malloc(sizeof(Node));
+    if(!node){
+        return NULL;
+    }
+    char* nodeString = (char*) malloc(strlen(string) + 1);
+    if(!nodeString){
+        free(node);
+        return NULL;
+    }
+    strcpy(nodeString, string);
+    node->element = nodeString;
+    node->amount = amount;
+    node->next = *nextPointer;
+    *nextPointer = node;
+    return node;
+}
+
 AmountSet asCopy(AmountSet set)
 {
-    AmountSet copy = (AmountSet) malloc(sizeof(AmountSet));
+    AmountSet copy = asCreate();
     if(!copy){
         return NULL;
     }
-    Node* source = set->first;
-    Node* destination;
-    while(source){
-        destination = (Node*) malloc(sizeof(Node));
-        if(!destination){
+    Node** destination = &copy->first;
+    for(Node* source = set->first; source; source = source->next){
+        Node* newNode = linkNewNode(destination, source->element, source->amount);
+        if(!newNode){
+            asDestroy(copy);
             return NULL;
         }
-        destination->amount = source->amount;
-        destination->element = strcpy(destination->element, source->element);
-        destination = destination->next;
-        source = source->next;
+        destination = &newNode->next;
     }
-    copy->first = destination;
     copy->length = set->length;
-    copy->currentIteration = NULL;
-    set->currentIteration = NULL;
     return copy;
 }
 
@@ -78,17 +90,22 @@ int asGetSize(AmountSet set)
     return set->length;
 }
 
-bool asContains(AmountSet set, const char* element)
-{
-    if(!set){
-        return false;
-    }
-    AS_FOREACH(char*, setElement, set){
-        if(!strcmp(element, setElement)){
-            return true;
+static Node** findNode(AmountSet set, const char* element){
+    Node** nextNodePointer = &set->first;
+    for(; *nextNodePointer; nextNodePointer = &(*nextNodePointer)->next){
+        if(!strcmp((*nextNodePointer)->element, element)){
+            break;
         }
     }
-    return false;
+    return nextNodePointer;
+}
+
+bool asContains(AmountSet set, const char* element)
+{
+    if(!set || !element){
+        return false;
+    }
+    return *findNode(set, element);
 }
 
 AmountSetResult asGetAmount(AmountSet set, const char* element, double* outAmount)
@@ -96,66 +113,12 @@ AmountSetResult asGetAmount(AmountSet set, const char* element, double* outAmoun
     if(!set || !element || !outAmount){
         return AS_NULL_ARGUMENT;
     }
-    int numberOfOccurrences = 0;
-    AS_FOREACH(char*, setElement, set){
-        if(!strcmp(setElement, element)){
-            numberOfOccurrences++;
-        }
-    }
-    if(numberOfOccurrences == 0){
+
+    Node** node = findNode(set, element);
+    if(!*node){
         return AS_ITEM_DOES_NOT_EXIST;
     }
-    *outAmount = numberOfOccurrences;
-    return AS_SUCCESS;
-
-}
-AmountSetResult addAtInit(AmountSet set, const char* element){
-    set->first = (Node*) malloc(sizeof(Node));
-    if(!set->first){
-        return AS_OUT_OF_MEMORY;
-    }
-    strcpy(set->first->element, element);
-    set->first->amount = 0;
-    return AS_SUCCESS;
-}
-
-AmountSetResult addAtBeginning(AmountSet set, const char* element){
-    Node* newFirst = (Node*) malloc(sizeof(Node));
-    if(!newFirst){
-        return AS_OUT_OF_MEMORY;
-    }
-    strcpy(newFirst->element, element);
-    newFirst->next = set->first;
-    newFirst->amount = 0;
-    set->first = newFirst;
-    return AS_SUCCESS;
-}
-
-AmountSetResult addAtEnd(Node* iterator, const char* element)
-{
-    if(!strcmp(iterator->element, element)){
-        return AS_ITEM_ALREADY_EXISTS;
-    }
-    iterator->next = (Node*) malloc(sizeof(Node));
-    if(!iterator->next){
-        return AS_OUT_OF_MEMORY;
-    }
-    strcpy(iterator->next->element, element);
-    iterator->next->amount = 0;
-    return AS_SUCCESS;
-}
-
-AmountSetResult addAfterCurrentNode(Node* current, const char* element){
-    if(!strcmp(current->next->element, element)){
-        return AS_ITEM_ALREADY_EXISTS;
-    }
-    Node* new = (Node*) malloc(sizeof(Node));
-    if(!new){
-        return AS_OUT_OF_MEMORY;
-    }
-    strcpy(new->element, element);
-    new->next = current->next;
-    current->next = new;
+    *outAmount = (*node)->amount;
     return AS_SUCCESS;
 }
 
@@ -164,79 +127,51 @@ AmountSetResult asRegister(AmountSet set, const char* element)
     if(!set || !element){
         return AS_NULL_ARGUMENT;
     }
-    if(!set->first) {
-        return addAtInit(set, element);
-    }
-    if(strcmp(element, set->first->element) < 0){
-        return addAtBeginning(set, element);
-    }
-    Node* iterator = set->first;
-    for(; iterator->next && strcmp(element, iterator->next->element) > 0; iterator = iterator->next);
-    if(!iterator->next){
-        return addAtEnd(iterator, element);
-    }
-    return addAfterCurrentNode(iterator, element);
-
-}
-Node* getNodeByElement(AmountSet set, const char* element){
-    Node* iterator = set->first;
-    while(iterator){
-        if(!strcmp(iterator->element, element)){
-            return iterator;
+    Node** nextNodePointer = &set->first;
+    for(; *nextNodePointer ;nextNodePointer = &(*nextNodePointer)->next){
+        int compareResult = strcmp((*nextNodePointer)->element, element);
+        if(compareResult == 0){
+            return AS_ITEM_ALREADY_EXISTS;
+        }
+        if (compareResult > 1){
+            break;
         }
     }
-    return iterator;
+    linkNewNode(nextNodePointer, element, 0);
+    set->length++;
+    return AS_SUCCESS;
 }
+
 AmountSetResult asChangeAmount(AmountSet set, const char* element, double amount)
 {
     if(!set || !element){
         return AS_NULL_ARGUMENT;
     }
-    Node* node = getNodeByElement(set, element);
-    if(!node){
+    Node** node = findNode(set, element);
+    if(!*node){
         return AS_ITEM_DOES_NOT_EXIST;
     }
-    if(amount > node->amount){
+    if(-amount > (*node)->amount){
         return AS_INSUFFICIENT_AMOUNT;
     }
-    node->amount += amount;
-    return AS_SUCCESS;
-}
-AmountSetResult deleteFirstElement(AmountSet set){
-    Node* toDelete = set->first;
-    set->first = set->first->next;
-    free(toDelete);
-    return AS_SUCCESS;
-}
-AmountSetResult deleteLastElement(Node* oneBeforeLast){
-    Node* last = oneBeforeLast->next;
-    oneBeforeLast->next = NULL;
-    free(last);
-    return AS_SUCCESS;
-}
-AmountSetResult deleteNextElement(Node* node){
-    Node* toDelete = node->next;
-    node->next = node->next->next;
-    free(toDelete);
+    (*node)->amount += amount;
     return AS_SUCCESS;
 }
 AmountSetResult asDelete(AmountSet set, const char* element)
 {
-    if(!set){
+    if(!set || !element){
         return AS_NULL_ARGUMENT;
     }
-    if(!strcmp(set->first->element, element)){
-        return deleteFirstElement(set);
-    }
-    Node* iterator = set->first;
-    for(; iterator->next && strcmp(element, iterator->next->element) > 0; iterator = iterator->next);
-    if(!iterator->next){
+    Node** previous = findNode(set, element);
+    Node* toDelete = *previous;
+    if(!toDelete) {
         return AS_ITEM_DOES_NOT_EXIST;
     }
-    if(!iterator->next->next){
-        return deleteLastElement(iterator);
-    }
-    return deleteNextElement(iterator);
+    *previous = toDelete->next;
+    free(toDelete->element);
+    free(toDelete);
+    set->length--;
+    return AS_SUCCESS;
 }
 
 AmountSetResult asClear(AmountSet set)
@@ -245,7 +180,7 @@ AmountSetResult asClear(AmountSet set)
         return AS_NULL_ARGUMENT;
     }
     while(set->first){
-        deleteFirstElement(set);
+        asDelete(set, set->first->element);
     }
     return AS_SUCCESS;
 }
